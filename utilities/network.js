dd87132f33095fcbc3ea56b4f3b4451f78993ceb
Autarc <Autarc@gmail.com>
2015-05-06 12:44:47 +0200

    create request helper

---
+++ utilities/network.js
@@ -0,0 +1,34 @@
+/**
+ * Network
+ * =======
+ *
+ *
+ */
+
+import Promise from 'bluebird'
+import request from 'request'
+
+/**
+ * [getData description]
+ *
+ * Wrap a request into a promise.
+ *
+ * @param  {[type]} url [description]
+ * @param  {[type]} qs  [description]
+ * @return {[type]}     [description]
+ */
+// Promise.promisify(request.get)
+export function getData (url, qs) {
+  return new Promise(function (resolve, reject) {
+    request.get({
+      url,
+      qs,
+      json: true
+    }, function (err, res, body) {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(body)
+    })
+  })
+}
