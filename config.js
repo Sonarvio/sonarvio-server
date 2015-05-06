1cd16a7241a91385791c60cc759f95aab98a658f
Autarc <Autarc@gmail.com>
2015-05-06 12:44:32 +0200

    define configuration

---
+++ config.js
@@ -0,0 +1,18 @@
+/**
+ * Config
+ * ======
+ *
+ * Settings and credentials for different platforms.
+ */
+
+export default {
+  server: {
+    host: 'localhost',
+    port: 9000,
+    address: null
+  },
+  services: {
+    acoustid: null,
+    mooma: null
+  }
+}
