0908887f782171b370108bb3bdcde57e423795dc
Autarc <Autarc@gmail.com>
2015-05-06 12:52:34 +0200

    add 'mooma' service

---
+++ services/mooma.js
@@ -0,0 +1,42 @@
+/**
+ * MooMa
+ * =====
+ *
+ * Retrieve matches for 'echoprints'.
+ *
+ * Service: http://www.mooma.sh/api.html
+ */
+
+import {getData} from '../utilities/network'
+import {services} from '../config'
+
+/**
+ * [mooma description]
+ *
+ *  TODO:
+ *  - allow selecting different service provider
+ *
+ * @param  {[type]} code    [description]
+ * @param  {[type]} service [description]
+ * @return {[type]}         [description]
+ */
+export default function mooma (code, service = null) {
+  var lookup = null
+  switch (service) {
+    case 'mooma':
+    default:
+      lookup = getData('http://api.mooma.sh/v1/song/identify', {
+        code,
+        api_key: services.mooma
+      })
+  }
+  return lookup.then(function (body) {
+    var songs = body.response.songs.map(function (song) {
+      return {
+        title: song.title,
+        artist: song.artist_name
+      }
+    })
+    return Promise.resolve(songs)
+  })
+}
